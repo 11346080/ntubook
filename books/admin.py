dc76078a8b9c9e847969c469aa5771203796aada
wesleybot <11346064@ntub.edu.tw>
2026-03-17 18:49:20 +0800

    recover ud error fix finish @X!aN

--- books/admin.py
+++ books/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from .models import Book, BookApplicability
 
-# class BookApplicabilityInline(admin.TabularInline):
-#     model = BookApplicability
-#     extra = 1
-# TODO: 效能問題隔離測試 - 先註冊 BookAdmin 觀察是否正常運作
+class BookApplicabilityInline(admin.TabularInline):
+    model = BookApplicability
+    extra = 1
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author_display', 'isbn13', 'publisher')
     search_fields = ('title', 'author_display', 'isbn13', 'isbn10')
-    # inlines = [BookApplicabilityInline]  # 暫時移除以測試效能問題
+    inlines = [BookApplicabilityInline] # 可以在編輯書本時，直接新增適用課程
