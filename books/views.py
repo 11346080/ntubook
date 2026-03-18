99be8aced00e3ff953440ace1bb871ce0319d154
wesleybot <11346064@ntub.edu.tw>
2026-03-18 11:32:00 +0800

    修改 - name_zh名稱使用錯誤、模板

--- books/views.py
+++ books/views.py
@@ -13,3 +13,8 @@ class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['applicabilities'] = self.object.applicabilities.all()
+        return context
