78444beac6fea8b1bb1b3c0645fb60ede05254c3
wesleybot <11346064@ntub.edu.tw>
2026-03-19 01:02:40 +0800

    修復 - 與上一個Q一樣

---
+++ purchase_requests/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PurchaseRequestsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "purchase_requests"
+    verbose_name = "預約請求管理"
